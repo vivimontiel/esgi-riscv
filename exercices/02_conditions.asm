c6f75e20140af45cc594c9af30189474c6e3c754
Viviana Montiel <vivimontiel@gmail.com>
2020-04-29 11:56:16 +0200

    updating exercises 02, 04 and 05

--- exercices/02_conditions.asm
+++ exercices/02_conditions.asm
@@ -11,15 +11,19 @@
 # Assigner des nombres dans des registres:
 # - `t0` <- 5
 # - `t1` <- 4
+#
+# Corriger les lignes avec des `???`.
 .data
+
 msg_eq: .asciz "Ces nombres sont egaux!"
 msg_neq: .asciz "Ces nombres ne sont pas egaux"
+
 .text
 
 # En rust on aurais surement fait un truc du genre
 # ```rust
-# let a = 5;
-# let b = 4;
+# let a = 4;
+# let b = 5;
 # let msg = if a == b { "Ces nombres sont egaux!" } else { "Ces nombres ne sont pas egaux" };
 # println!(msg);
 # ```
@@ -27,26 +31,30 @@ msg_neq: .asciz "Ces nombres ne sont pas egaux"
 ### Ici à vous de jouer utiliser
 # deux instructions à écrire pour charger 5 dans t0 et t1 dans 4.
 
-li t0, 5
-li t1, 4
+# début du programme.
+main:
+li t0, 4
+li t1, 5
 
+# "bne" Branch if Not Equals: Saute si nos registres ne sont pas égaux.
 bne t0, t1, not_eq # if t0 != t1 jump not_eq
 
-# Cas égaux
-la a0, msg_eq # msg <- "Ces nombres sont egaux!" 
+# Cas égaux t0 == t1
+la a0, msg_eq # msg <- "Ces nombres sont egaux!"
 j end # On a fini donc on peut sortir du if.
 
-# Cas Inégaux
+# Cas Inégaux t0 != t1
 not_eq:
 la a0 msg_neq # msg <- "Ces nombres ne sont pas egaux"
 j end
 
 end:
-# Affichage
+
+# Affichage:
 #
 # Ici on souhaite afficher sur la console via un appel système.
-# inspirez vous de l'exercice 01_syscall, le registre a7 doit contenir
+# inspirez vous de l'exercice 01_syscall, le registre a0 doit contenir
 # le numéro du syscall printString: 4 et a7 la chaine à afficher.
 
-li a7, 4 
-ecall
+li a7, 4  # numéro syscall
+ecall     # Quelle instruction doit-on utiliser pour faire un syscall?
--- exercices/04_array.asm
+++ exercices/04_array.asm
@@ -1,11 +1,24 @@
 # Exercice 3
 #
 # Objectif: Additionner toutes les cases d'un tableau version «naive»
+# Sans boucle, ça correspond à faire l'addition de array_g[0] + array_g[1] + array_g[2].
 #
-# A faire: complêter le programme à trou.
+# A faire: compléter le programme à trou.
 #
 # Questions:
-# A venir
+#
+# - Que fait l'instruction lw: load word?
+# - lw: À quoi sert l'immédiat devant le registre contenant l'adresse?
+# - Que va contenir le registre de destination de lw?
+#
+# - Avec une instruction comme add à quoi correspondent les trois registres?
+# `add reg0, reg1, reg2`
+#   - reg0:
+#   - reg1:
+#   - reg2:
+#
+# - `la` sert à quoi?
+# - Quand on travail sur des mots de 32 bits les addresses sont allignées de combien en combien?
 
 .data
 # /!\ Attention! ce sont des variables globales en mémoire! /!\
@@ -13,26 +26,51 @@
 # On déclare «naivement» un tableau et sa taille, dans le segment data.
 # On vera dans la suite du cours que c'est pas exactement comme ça qu'on fait,
 # dans la vrai vie des choses de font dans la pile.
-size_array_g: .word 6
-array_g: .word 124, 256, 512, 2, 8, 4
+size_array_g: .word 3
+array_g: .word 124, 256, 512
+
+# En mémoire ça resemble à ça @ référe dans ce schema a l'addresse
+# de base du tableau.
+# +----------+-------+---------------+
+# | Addresse | @ + 0 | @ + 4 | @ + 8 |
+# +----------+-------+-------+-------+
+# | Contenu  |  124  |  256  |  512  |
+# +----------+-------+-------+-------+
+
+# Chaines pour les test
+msg_success: .asciz "La somme correspond bien! Bravo."
+msg_fail: .asciz "Whops c'est pas encore ça, courage! :)"
 
 .text
 
 main:
 
-# On souhaite faire la somme du tableau! on va faire la méthode bourrin
-# ou on additionne toute les cases dans un registre.
-# load word depuis l'addresse 0 + register dans le register t1
-lw  t1, 0(t0)
+la t0, array_g # t0 contiens l'addresse du tableau
 
-# t2 <- @num1
-l?  t2,  ???
-# Quelle instruction permet de charger un mot mémoire?
-??? t3, 0(t2)
+# Pour faire la somme du tableau on va additionner manuellement,
+# le contenu de toutes les cases du tableau.
 
-# chargement de l'addresse du resultat.
-la t4, ???
-# fait l'addition entre nos deux registres dans un registre.
-add ???, ???, ???
-# store word, stock le contenu du registre dans l'addresse à 0 + t4
+lw  t1, 0(t0) # t1: array_g[0]
+# lw ???, 4(???) <- TODO: À completer!
+# Vous pouvez aussi faire une addition sur l'addresse qui est dans t0
+add a0, t1, t2 # sum = t2 + t1
 
+
+# Ici il vous reste à additionner a0 qui contiens la somme
+# avec le registre qui contiendra la valeur de la 3ème case du tableau.
+
+###
+# Partie prof test si ça fonctionne et affiche un message en fonction.
+###
+test:
+# Test sum == 892
+li  a1, 892
+bne a0, a1, fail_test # if a0 !t1 then fail_test
+la a0, msg_success
+j print
+fail_test:
+la a0, msg_fail
+
+print:
+li a7, 4
+ecall
